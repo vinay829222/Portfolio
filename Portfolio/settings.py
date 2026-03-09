b9bb255039334cb1bd30ce0217ce3dd95bedca5c
VinayKumar <Vinaylkr2@gmail.com>
2026-03-10 00:06:58 +0530

    Configure Django project for Vercel deployment
    - Add Vercel entry point (api/index.py)
    - Update settings for production (DEBUG=False, ALLOWED_HOSTS)
    - Add WhiteNoise for static file serving
    - Update requirements.txt with whitenoise
    - Collect static files
    - Add .gitignore for Django project

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kuphaeyizs(_pl)vg$fmx!(7)poqex3xjg380i2ofqz+*%2x%t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
